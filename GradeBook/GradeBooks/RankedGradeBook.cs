b2b8102948b8a89404d1e8e1a8e5781a12bff9b4
rbibler <rbibler01@hotmail.com>
2018-10-25 19:58:44 -0400

    Completed Module 3

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -1,6 +1,7 @@
 ﻿using GradeBook.Enums;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace GradeBook.GradeBooks
@@ -11,5 +12,42 @@ namespace GradeBook.GradeBooks
         {
             Type = GradeBookType.Ranked;
         }
+
+        
+        public override char GetLetterGrade(double averageGrade)
+        {
+            if (Students.Count < 5)
+            {
+                throw new InvalidOperationException();
+            }
+
+            var threshold = (int) Math.Ceiling(Students.Count * 0.2);
+            var grades = Students.OrderByDescending(s => s.AverageGrade).Select(s => s.AverageGrade).ToList();
+
+            if (grades[threshold - 1] <= averageGrade)
+            {
+                return 'A';
+            }
+            else if (grades[threshold * 2] - 1 <= averageGrade)
+            {
+                return 'B';
+            }
+            else if (grades[threshold * 3] - 1 <= averageGrade)
+            {
+                return 'C';
+            }
+            else if (grades[threshold * 4] - 1 <= averageGrade)
+            {
+                return 'D';
+            }
+            else
+            {
+                return 'F';
+            }
+
+
+        }
+
+
     }
 }
