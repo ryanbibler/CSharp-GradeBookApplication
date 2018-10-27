aae716e63680b8e289f1322408c3ab77617beb90
rbibler <rbibler01@hotmail.com>
2018-10-27 10:58:54 -0400

    Completed module 6

--- GradeBook/UserInterfaces/StartingUserInterface.cs
+++ GradeBook/UserInterfaces/StartingUserInterface.cs
@@ -40,7 +40,7 @@ namespace GradeBook.UserInterfaces
             }
             var name = parts[1];
             var type = parts[2];
-            var gradeBook = new BaseGradeBook(name);
+            BaseGradeBook gradeBook;
             if (type == "standard")
             {
                 gradeBook = new StandardGradeBook(name);
